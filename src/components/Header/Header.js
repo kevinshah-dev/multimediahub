075c0d814e6fc87fb73899fd952b4cc23a34ebaf
Kevin Shah <ks539@duke.edu>
2023-05-18 17:05:56 -0400

    Added some comments

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
+//Contains the header seen at the top of the main url (https://multimediahub.netlify.app/)
+
+//Clicking on the header (Multimedia Hub) scrolls to the top of the page
 import "./Header.css";
 
 const Header = () => {
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header">
+    <span onClick={() => window.scroll(0, 0)} className="header">  
       Multimedia Hub 
     </span>
   );
